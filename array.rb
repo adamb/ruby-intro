37d10d9f2ffce901e3640f394e7119b0272de8fe
Adam Beguelin <adamb@dao.local>
2008-11-11 13:14:37 -0800

    add array and string examples

---
+++ array.rb
@@ -0,0 +1,23 @@
+# some examples of arrays
+
+  a = [1,2,3,4]
+  
+  puts "array a:"
+  a.each { |e| puts e }
+  
+  b = Array.new
+  
+  b << 1
+  b << 2
+  b << 3
+  
+  puts "\narray b:"
+  b.each { |e| puts e }
+  
+  c = %w[Tahoe Donner Incline Jobs Sierra]
+  puts "\narray c:"
+  c.each { |e| puts e }
+  
+  d = a + c
+  puts "\narray d:"
+  d.each { |e| puts e }
---
+++ string.rb
@@ -0,0 +1,7 @@
+  say = "I love Ruby"
+  puts say
+  
+  say['love'] = "*love*"
+  puts say.upcase
+  
+  5.times {puts say}
